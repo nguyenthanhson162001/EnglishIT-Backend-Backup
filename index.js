8ff505d4d2937b7a1f4fc5a25c4a26f3a20151f6
nguyenthanhson162001 <nguyenthanhson162001@gmail.com>
2022-01-08 15:02:58 +0700

    https

--- index.js
+++ index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 require('dotenv').config()
 var morgan = require('morgan') // show log
-
-
 const routes = require('./routes');
 const cors = require('cors')
 const port = 3004;
+var os = require("os");
+
 const { auth, requiresAuth } = require("express-openid-connect");
 const got = require("got");
 const Vocabulary = require('./app/model/vocabulary')
@@ -15,6 +15,18 @@ app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencode
 const server = require('./config/socket/exam')(app)
 app.use(morgan('combined'))
+
+
+// https congfig
+const https = require('https')
+const fs = require('fs')
+const path = require('path')
+
+const sslServer = https.createServer({
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'cert.pem')),
+}, app)
+
 
 var corsOptions = {
     origin: process.env.URL_CLIENT,
@@ -26,8 +38,9 @@ app.use(express.static('public'));
 
 routes(app)
 
+// server.listen(port, () => console.log(`server run with http://localhost:${port}`));
+sslServer.listen(port, () => console.log(`server run with https://${os.hostname()}:${port}`));
 
-server.listen(port, () => console.log(`server run with http://localhost:${port}`));
 
 // const xlsxFile = require('read-excel-file/node');
 
